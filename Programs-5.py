dc1c4c77133f4fcac2f14b89164dac31ee86d6ec
priyankabadagavi101-png <priyankabadagavi101@gmail.com>
2026-03-29 23:38:58 +0530

    Update Programs-5.py

--- Programs-5.py
+++ Programs-5.py
@@ -1,229 +1,229 @@
 # def message(): #called function 
-#     print("Hello world!")
-# message() #calling function
-# message()
+    print("Hello world!")
+message() #calling function
+message()
 
 # def login():
-#     username = input("Enter your username:")  
-#     password = input("Enter your password:")  
-#     if username == password:  
-#         print("login successfully")   
-#     else:  
-#         print("you have entered wrong credentials")  
+    username = input("Enter your username:")  
+    password = input("Enter your password:")  
+    if username == password:  
+        print("login successfully")   
+    else:  
+        print("you have entered wrong credentials")  
 
 # login()#calling function
 
-# def add():
-#     return 2+3
+def add():
+    return 2+3
 
-# print(add())
+print(add())
 
-# def add():
-#     return 2+3
+def add():
+    return 2+3
 
-# result=add()
-# print("Result=",result)
+result=add()
+print("Result=",result)
 
 
-#can a function return multiple value in python
-# def add():
-#     a=int(input("Enter value of a:"))
-#     b=int(input("Enter value of b:"))
-#     add=a+b
-#     sub=a-b
-#     mul=a*b
-#     div=a/b
-#     return add, sub, mul, div #it will return in Tuple
-# print(add())
+# can a function return multiple value in python
+def add():
+    a=int(input("Enter value of a:"))
+    b=int(input("Enter value of b:"))
+    add=a+b
+    sub=a-b
+    mul=a*b
+    div=a/b
+    return add, sub, mul, div #it will return in Tuple
+print(add())
 
-# def add():
-    # a=int(input("Enter value of a:"))
-    # b=int(input("Enter value of b:"))
-#     add=a+b
-#     sub=a-b
-#     mul=a*b
-#     div=a/b
-#     return add, sub, mul, div #it will return in Tuple
+def add():
+    a=int(input("Enter value of a:"))
+    b=int(input("Enter value of b:"))
+    add=a+b
+    sub=a-b
+    mul=a*b
+    div=a/b
+    return add, sub, mul, div #it will return in Tuple
 
-# result=add()
-# print("Result=",result)
-# print(result)
+result=add()
+print("Result=",result)
+print(result)
 
 # How many types of arguments we can pass in function 
-# ==>there are 4 types of arguments we can pass in funcction
-# 1.Positional argument
-# 2.keyword argument
-# 3.default argument
-# 4. variable number of argument or variable length of argument
+==>there are 4 types of arguments we can pass in funcction
+1.Positional argument
+2.keyword argument
+3.default argument
+4. variable number of argument or variable length of argument
 
-#Positional argument
-# def name(a,b):
-#     print(a+b)
+# Positional argument
+def name(a,b):
+    print(a+b)
 
-# name(2,3)
+name(2,3)
 
 # def personInfo(fname,lname):
-#     print("First name=",fname)
-#     print("Last name=",lname)
+    print("First name=",fname)
+    print("Last name=",lname)
 
-# personInfo("prashant","jha")
+personInfo("prashant","jha")
 
-#keywaord argument
-# def profile(fname,lname):
-#     print("first name=",fname)
-#     print("Last name=",lname)
+# keywaord argument
+def profile(fname,lname):
+    print("first name=",fname)
+    print("Last name=",lname)
 
-# profile(fname="prashant",lname="jha")
+profile(fname="prashant",lname="jha")
 
-#Default argument
-# def cityName(city): #without argument
-#     print("City Name=",city)
+# Default argument
+def cityName(city): #without argument
+    print("City Name=",city)
 
-# cityName("Nagpur")
-# cityName("Delhi")
-# cityName()
+cityName("Nagpur")
+cityName("Delhi")
+cityName()
 
-# def cityName(city="Banglore"): #with argument
-#     print("City Name=",city) 
+def cityName(city="Banglore"): #with argument
+    print("City Name=",city) 
 
-# cityName("Nagpur")
-# cityName("Delhi")
-# cityName()
+cityName("Nagpur")
+cityName("Delhi")
+cityName()
 
-#variable number of arguments/variable length of arguments
-# def city(*name):
-#     print(name)
-# city("Delhi", "Nagpur", "Mumbai", "Pune", "Banglore")
+# variable number of arguments/variable length of arguments
+def city(*name):
+    print(name)
+city("Delhi", "Nagpur", "Mumbai", "Pune", "Banglore")
 
-#WAP to do amenu friven program using arithmetic operation
-# import sys
-# def addition():
-#     a=int(input("Enter value of a:"))
-#     b=int(input("Enter value of b:"))
-#     print("Addition=",a+b)
+# WAP to do amenu friven program using arithmetic operation
+import sys
+def addition():
+    a=int(input("Enter value of a:"))
+    b=int(input("Enter value of b:"))
+    print("Addition=",a+b)
 
-# def substraction():
-#     a=int(input("Enter value of a:"))
-#     b=int(input("Enter value of b:"))
-#     print("substraction=",a-b)
+def substraction():
+    a=int(input("Enter value of a:"))
+    b=int(input("Enter value of b:"))
+    print("substraction=",a-b)
 
-# def Multiplication():
-#     a=int(input("Enter value of a:"))
-#     b=int(input("Enter value of b:"))
-#     print("Multiplication=",a*b)
+def Multiplication():
+    a=int(input("Enter value of a:"))
+    b=int(input("Enter value of b:"))
+    print("Multiplication=",a*b)
 
-# def Division():
-#     a=int(input("Enter value of a:"))
-#     b=int(input("Enter value of b:"))
-#     print("Division=",a/b)
+def Division():
+    a=int(input("Enter value of a:"))
+    b=int(input("Enter value of b:"))
+    print("Division=",a/b)
 
-# while True:
-#     print("1. Addition")
-#     print("2. substraction") 
-#     print("3. Multiplicaton")
-#     print("4. Division")
-#     print("5. Exit")
-#     choice=(int(input("Enter your choice:")))
-#     if choice==1:
-#         addition()
-#     elif choice==2:
-#         substraction()
-#     elif choice==3:
-#         Multiplication()
-#     elif choice==4:
-#         Division()
-#     elif choice==5:
-#         sys.exit()
+while True:
+    print("1. Addition")
+    print("2. substraction") 
+    print("3. Multiplicaton")
+    print("4. Division")
+    print("5. Exit")
+    choice=(int(input("Enter your choice:")))
+    if choice==1:
+        addition()
+    elif choice==2:
+        substraction()
+    elif choice==3:
+        Multiplication()
+    elif choice==4:
+        Division()
+    elif choice==5:
+        sys.exit()
 
-#Calculate factorial
-# # def fact(n):
-#     if n==0 or n==1:
-#         return 1
-#     else:
-#         return n*fact(n-1)
-# num=int(input("Enter a number:"))
-# result=fact(num)
-# print("Factorial of",num,"is",result)
+# Calculate factorial
+# def fact(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*fact(n-1)
+num=int(input("Enter a number:"))
+result=fact(num)
+print("Factorial of",num,"is",result)
 
-##Check primre number
-# import math
+#Check primre number
+import math
 
-# def is_prime(n):
-#     if n <= 1:
-#         return False   
-#     for i in range(2, int(math.sqrt(n)) + 1):
-#         if n % i == 0:
-#             return False   
-#     return True
-# num = int(input("Enter a number: "))
-# if is_prime(num):
-#     print(num, "is a Prime number")
-# else:
-#     print(num, "is NOT a Prime number") 
+def is_prime(n):
+    if n <= 1:
+        return False   
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False   
+    return True
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(num, "is a Prime number")
+else:
+    print(num, "is NOT a Prime number") 
          
 
-# try:
-#     a=int(input("Enter the value of a:"))
-#     b=int(input("Enter the value of b:"))
-#     print(a/b)
-# except ZeroDivisionError:
-#     print("can't devide be zero")
-# except ValueError:
-#     print("Enter only integer value")
+try:
+    a=int(input("Enter the value of a:"))
+    b=int(input("Enter the value of b:"))
+    print(a/b)
+except ZeroDivisionError:
+    print("can't devide be zero")
+except ValueError:
+    print("Enter only integer value")
 
-#Handling multiple different kinds of exception with 
-#Single except block
-# try:
-#     a=int(input("Enter the value of a:"))
-#     b=int(input("Enter the value of b:"))
-#     print(a/b)
-# except (ValueError, ZeroDivisionError)as message:
-#     print(message)
+# Handling multiple different kinds of exception with 
+# Single except block
+try:
+    a=int(input("Enter the value of a:"))
+    b=int(input("Enter the value of b:"))
+    print(a/b)
+except (ValueError, ZeroDivisionError)as message:
+    print(message)
 
-#The concept of default except block, generally we used for
-#Writing normal message or showing normal error
-# try:
-#     a=int(input("Enter the value of a:"))
-#     b=int(input("Enter the value of b:"))
-#     print(a/b)
-# except (ValueError, ZeroDivisionError)as message:
-#     print("Enter the correct number:", message)
-# except:
-#     print("Ths is default part of the except block")
-
-
-# try:
-#     a=int(input("Enter the value of a:"))
-#     b=int(input("Enter the value of b:"))
-#     print(a/b)
-# except (ValueError, ZeroDivisionError)as message:
-#     print("Enter the correct number:", message)
-# except:
-#     print("Ths is default part of the except block")
-# else:
-#     print("Everything is ok")
+# The concept of default except block, generally we used for
+# Writing normal message or showing normal error
+try:
+    a=int(input("Enter the value of a:"))
+    b=int(input("Enter the value of b:"))
+    print(a/b)
+except (ValueError, ZeroDivisionError)as message:
+    print("Enter the correct number:", message)
+except:
+    print("Ths is default part of the except block")
 
 
-#Finally block will always executed whether try block generate error or not
-# try:
-#     a=int(input("Enter the value of a:"))
-#     b=int(input("Enter the value of b:"))
-#     print(a/b)
-# except (ValueError, ZeroDivisionError)as message:
-#     print("Enter the correct number:", message)
-# finally:
-#     print("I will always executed")
+try:
+    a=int(input("Enter the value of a:"))
+    b=int(input("Enter the value of b:"))
+    print(a/b)
+except (ValueError, ZeroDivisionError)as message:
+    print("Enter the correct number:", message)
+except:
+    print("Ths is default part of the except block")
+else:
+    print("Everything is ok")
 
-#nested try except block
-# try:
-#     a=int(input("Enter first number:"))
-#     b=int(input("Enter second number:"))
-#     try:
-#         print(a/b)
-#     except ZeroDivisionError as msg:
-#         print(msg)
-# except ValueError as msg:
-#     print(msg)
+
+# Finally block will always executed whether try block generate error or not
+try:
+    a=int(input("Enter the value of a:"))
+    b=int(input("Enter the value of b:"))
+    print(a/b)
+except (ValueError, ZeroDivisionError)as message:
+    print("Enter the correct number:", message)
+finally:
+    print("I will always executed")
+
+nested try except block
+try:
+    a=int(input("Enter first number:"))
+    b=int(input("Enter second number:"))
+    try:
+        print(a/b)
+    except ZeroDivisionError as msg:
+        print(msg)
+except ValueError as msg:
+    print(msg)
 
 
 #user defined exception by raise keyword
